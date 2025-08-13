dac61f2fc342a45a3837af4e6e3ce01267c8b7d7
lsw1232414 <limsw980821@gmail.com>
2025-08-13 09:53:16 +0900

    hw

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,20 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "./index.css";
 import Home from "./compo/Home";
 import Layout from "./compo/Layout";
 import Work1 from "./compo/Work1";
 import Work2 from "./compo/Work2";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter basename="/react-site">
       <Routes>
-        {/* 홈 — 홈 UI 전체를 Home.jsx에 넣어서 관리 */}
-        <Route path="/react-site" element={<Layout />}>
+        <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
+          <Route path="work1" element={<Work1 />} />
+          <Route path="work2" element={<Work2 />} />
         </Route>
-
-        {/* Work1 — 단독 화면 */}
-        <Route path="/react-site/work1" element={<Work1 />} />
-        <Route path="/react-site/work2" element={<Work2 />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
-
 export default App;
